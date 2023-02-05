55d7027734e0780f33e037673777faf5a92cd5fc
Eduardo Oropeza <josee375@outlook.com>
2023-02-05 16:40:02 -0600

    Funcionamiento del Relay
    Funcionamiento del Relay establecido a menor 27

---
+++ conjunto_relay/conjunto_relay.ino
@@ -0,0 +1,85 @@
+//librerias
+
+#include <WiFi.h>
+#include "FirebaseESP32.h"
+#include "DHT.h"
+//pines 
+
+#define DHTTYPE DHT11   
+#define DHTPIN 23
+const int humsuelo = 33;
+int bombaAgua = 2;
+//variables entorno
+int valHumsuelo;
+const char* ssid = "TP-Link_33A2";
+const char* pass = "14642814";
+
+//credenciales proyecto Firebase
+#define FIREBASE_HOST "ardu-cfdaa-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "120UVzgFFNp6gzjg4YrQvW0wA6gWDBZ2ya5I8iKR"
+
+//Firebase Data Object
+WiFiClient client; 
+FirebaseData firebaseData;
+String ruta = "Ejemplo_01";
+
+DHT dht(DHTPIN, DHTTYPE);
+ double h;
+ double t;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+ Serial.println("DHTxx test!");
+ WiFi.begin(ssid, pass);
+ Serial.print("Se está conectando a la red WiFi denominada ");
+ Serial.println(ssid);
+  /*Mientras se realiza la conexión a la red, aparecerán puntos, hasta que se realice la conexión*/
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+ pinMode(humsuelo, INPUT);
+ dht.begin();
+ pinMode(bombaAgua, OUTPUT);
+ Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+ Firebase.reconnectWiFi(true);
+   if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+   }
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+ h = dht.readHumidity();
+ t = dht.readTemperature();
+ Serial.println("Humedad: ");
+   Serial.println(h);
+   Serial.println(", Temperatura: ");
+   Serial.println(t);
+   Serial.println();
+   valHumsuelo = map(analogRead(humsuelo),4092,0,0,100);
+  Serial.println("Humedad del suelo");
+  Serial.println(valHumsuelo);
+  if(t<=27&&valHumsuelo<=20){
+     digitalWrite(bombaAgua, LOW);
+     delay(4000);
+     Serial.println("Bomba activada");
+     Firebase.setString(firebaseData, ruta+ "/Mensaje","Sistema de riego activado");
+  }else{
+    digitalWrite(bombaAgua, HIGH);
+    Serial.println("apagado");
+    Firebase.setString(firebaseData, ruta+ "/Mensaje","Sistema de riego apagado");
+  }
+  
+Firebase.setInt(firebaseData, ruta+ "/Temperatura",t);
+Firebase.setInt(firebaseData, ruta+ "/Humedad", h);  
+Firebase.setInt(firebaseData, ruta+ "/Humedad Tierra",valHumsuelo);
+
+}
